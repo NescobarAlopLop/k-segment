976c03830d520d1a38287bf31d43607cca575d6d
GeorgeKesaev <kesaev@gmail.com>
2018-11-18 18:02:28 +0200

    removed duplicated line

--- spark_coreset-master/tests/stream.py
+++ spark_coreset-master/tests/stream.py
@@ -18,7 +18,6 @@ class Stream(object):
         self.last_leaf = []
         self.coreset_size = coreset_size
         self.stack = Stack()
-        self.coreset_size = coreset_size
         self.k = k
 
     def _merge(self, pset1, pset2):
