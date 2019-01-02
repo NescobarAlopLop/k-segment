41e2823a1a4d0934336800ce5ca12fb36b750480
GeorgeKesaev <kesaev@gmail.com>
2019-01-02 23:45:36 +0200

    pyspark examples

---
+++ examples/csv_streamer.py
@@ -0,0 +1,28 @@
+import socket
+from time import sleep
+
+host = 'localhost'
+port = 9990
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.bind((host, port))
+s.listen(1)
+while True:
+    print('\nListening for a client at',host , port)
+    conn, addr = s.accept()
+    print('\nConnected by', addr)
+    try:
+        print('\nReading file...\n')
+        with open('/home/ge/k-segment/datasets/KO.csv') as f:
+            for line in f:
+                out = line.encode('utf-8')
+                print('Sending line',line)
+                conn.send(out)
+                sleep(0.02)
+            print('End Of Stream.')
+    except socket.error:
+        print ('Error Occured.\n\nClient disconnected.\n')
+    finally:
+        conn.close()
+        s.close()
+
