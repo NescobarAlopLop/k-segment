badeaf9671a1bb15fb20ae6cd2998e7aa792c198
louiscyphre <goldman.sign.and.verify@gmail.com>
2019-05-06 18:57:16 +0300

    Dividers.py updated

--- k_segment_implementation/Dividers.py
+++ k_segment_implementation/Dividers.py
@@ -32,26 +32,27 @@ class Dividers:
 
     def __repr__(self):
         return "Dividers({}, {}, {}, {}, {}, {})".format(self.cost, self.begin, self.end,
-                                                     self.approx_coefficients, self.sub_divs, self.repr_idx)
-
-    def get_dividers_point_pairs_for_drawing(self):
-        log.debug('{}, {}, {}'.format(this_file_name(), this_func_name(), inspect.currentframe().f_lineno))
-        result = []
-        for i in self.sub_divs:
-            for j in i.sub_divs:
-                x1y1 = (i.begin, j.begin)
-                x1y2 = (i.begin, j.end)
-                x2y1 = (i.end, j.begin)
-                x2y2 = (i.end, j.end)
-
-                line1 = (x1y1, x1y2)
-                line2 = (x2y1, x2y2)
-                line3 = (x1y1, x2y1)
-                line4 = (x1y2, x2y2)
-
-                result.extend([line1, line2, line3, line4])
-        return result
+                                                         self.approx_coefficients, self.sub_divs, self.repr_idx)
 
     def update_cost(self, cost):
         log.debug('{}, {}, {}'.format(this_file_name(), this_func_name(), inspect.currentframe().f_lineno))
         self.cost = cost
+
+
+def get_dividers_point_pairs_for_drawing(dividers: List["Dividers"]):
+    log.debug('{}, {}, {}'.format(this_file_name(), this_func_name(), inspect.currentframe().f_lineno))
+    result = []
+    for i in dividers:
+        for j in i.sub_divs:
+            x1y1 = (i.begin, j.begin)
+            x1y2 = (i.begin, j.end)
+            x2y1 = (i.end, j.begin)
+            x2y2 = (i.end, j.end)
+
+            line1 = (x1y1, x1y2)
+            line2 = (x2y1, x2y2)
+            line3 = (x1y1, x2y1)
+            line4 = (x1y2, x2y2)
+
+            result.extend([line1, line2, line3, line4])
+    return result
