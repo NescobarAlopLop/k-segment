dc20ee486bfffefef41d19149672e9e031f212da
GeorgeKesaev <kesaev@gmail.com>
2019-05-14 18:54:10 +0300

    bar code test

--- k_segment_implementation/test_bicriteria.py
+++ k_segment_implementation/test_bicriteria.py
@@ -111,3 +111,24 @@ class UtilsTest(unittest.TestCase):
             y = [line[0][1], line[1][1]]
             plt.plot(x,y, linewidth=1, color='red')
         plt.show()
+
+    def test_with_bar_code(self):
+        img_file = misc.imread('/home/ge/k-segment/datasets/bar_code_1.png')
+        flat_img = np.mean(img_file, axis=2)
+        k = 1
+        depth = 2
+
+        data2 = np.asarray(flat_img)
+
+        _, _, divs = bicriteria(data2, k, depth, best_fit_line_and_cost, calc_best_fit_line_polyfit)
+        points = get_dividers_point_pairs_for_drawing(divs)
+        plt.figure()  # (figsize=(16, 9), dpi=200)
+        plt.grid(True)
+
+        plt.imshow(img_file)
+        for line in points:
+            # print(line)
+            x = [line[0][0], line[1][0]]
+            y = [line[0][1], line[1][1]]
+            plt.plot(x,y, linewidth=1, color='red')
+        plt.show()
