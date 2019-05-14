203b2a16861681cc5e976590e33a4729765eb2ee
GeorgeKesaev <kesaev@gmail.com>
2019-05-14 18:42:05 +0300

    biciteria banana test passed

--- k_segment_implementation/test_bicriteria.py
+++ k_segment_implementation/test_bicriteria.py
@@ -5,7 +5,7 @@ from k_segment_implementation.bicriteria import bicriteria
 from utils_seg import best_fit_line_and_cost, calc_best_fit_line_polyfit
 from Dividers import get_dividers_point_pairs_for_drawing
 import matplotlib.pyplot as plt
-
+from scipy import misc
 
 class UtilsTest(unittest.TestCase):
 
@@ -64,8 +64,50 @@ class UtilsTest(unittest.TestCase):
 
         plt.imshow(data2)
         for line in points:
-            print(line)
+            # print(line)
             x = [line[0][0], line[1][0]]
             y = [line[0][1], line[1][1]]
             plt.plot(x,y, linewidth=4.4, color='red')
+        plt.show()
+
+    def test_with_img(self):
+        img_file = misc.imread('/home/ge/k-segment/k_segment_implementation/colour_grid.png')
+        img_file = np.mean(img_file, axis=2)
+        k = 2
+        depth = 2
+
+        data2 = np.asarray(img_file)
+
+        _, _, divs = bicriteria(data2, k, depth, best_fit_line_and_cost, calc_best_fit_line_polyfit)
+        points = get_dividers_point_pairs_for_drawing(divs)
+        plt.figure()  # (figsize=(16, 9), dpi=200)
+        plt.grid(True)
+
+        plt.imshow(data2)
+        for line in points:
+            # print(line)
+            x = [line[0][0], line[1][0]]
+            y = [line[0][1], line[1][1]]
+            plt.plot(x,y, linewidth=1, color='red')
+        plt.show()
+
+    def test_with_banana(self):
+        img_file = misc.imread('/home/ge/k-segment/datasets/2018-04-09_BA_tree_15.JPG')
+        flat_img = np.mean(img_file, axis=2)
+        k = 2
+        depth = 2
+
+        data2 = np.asarray(flat_img)
+
+        _, _, divs = bicriteria(data2, k, depth, best_fit_line_and_cost, calc_best_fit_line_polyfit)
+        points = get_dividers_point_pairs_for_drawing(divs)
+        plt.figure()  # (figsize=(16, 9), dpi=200)
+        plt.grid(True)
+
+        plt.imshow(img_file)
+        for line in points:
+            # print(line)
+            x = [line[0][0], line[1][0]]
+            y = [line[0][1], line[1][1]]
+            plt.plot(x,y, linewidth=1, color='red')
         plt.show()
