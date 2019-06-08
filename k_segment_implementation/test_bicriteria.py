f0d36c45735dbb40cfe39ccc623f9b83ed53b43c
GeorgeKesaev <kesaev@gmail.com>
2019-06-08 20:36:23 +0300

    misc to imageio

--- k_segment_implementation/test_bicriteria.py
+++ k_segment_implementation/test_bicriteria.py
@@ -5,11 +5,11 @@ from k_segment_implementation.bicriteria import bicriteria
 from utils_seg import best_fit_line_and_cost, calc_best_fit_line_polyfit
 from Dividers import get_dividers_point_pairs_for_drawing
 import matplotlib.pyplot as plt
-from scipy import misc
+
 
 class UtilsTest(unittest.TestCase):
-
-    def test_bicriteria_one_dim_data(self):
+    @staticmethod
+    def test_bicriteria_one_dim_data():
         k = 2
         depth = 1
         data1 = np.asarray([
@@ -27,7 +27,8 @@ class UtilsTest(unittest.TestCase):
             plt.plot(x,y)
         plt.show()
 
-    def test_bicriteria_two_dim_data(self):
+    @staticmethod
+    def test_bicriteria_two_dim_data():
         k = 2
         depth = 2
         data2 = [[10, 10, 10, 11, 10, 11, 10, 14, 10, 10, 10, 12, 10, 15, 10, 12, 10, 13, 10, 16, 10, 10, 10, 13],
@@ -70,8 +71,9 @@ class UtilsTest(unittest.TestCase):
             plt.plot(x,y, linewidth=4.4, color='red')
         plt.show()
 
-    def test_with_img(self):
-        img_file = misc.imread('/home/ge/k-segment/k_segment_implementation/colour_grid.png')
+    @staticmethod
+    def test_with_img():
+        img_file = imageio.imread('/home/ge/k-segment/k_segment_implementation/colour_grid.png')
         img_file = np.mean(img_file, axis=2)
         k = 2
         depth = 2
@@ -91,8 +93,9 @@ class UtilsTest(unittest.TestCase):
             plt.plot(x,y, linewidth=1, color='red')
         plt.show()
 
-    def test_with_banana(self):
-        img_file = misc.imread('/home/ge/k-segment/datasets/2018-04-09_BA_tree_12.JPG')
+    @staticmethod
+    def test_with_banana():
+        img_file = imageio.imread('/home/ge/k-segment/datasets/segmentation/2018-04-09_BA_tree_12.JPG')
         flat_img = np.mean(img_file, axis=2)
         k = 3
         depth = 2
@@ -112,8 +115,9 @@ class UtilsTest(unittest.TestCase):
             plt.plot(x,y, linewidth=1, color='red')
         plt.show()
 
-    def test_with_bar_code(self):
-        img_file = misc.imread('/home/ge/k-segment/datasets/bar_code.png')
+    @staticmethod
+    def test_with_bar_code():
+        img_file = imageio.imread('/home/ge/k-segment/datasets/segmentation/bar_code.png')
         flat_img = np.mean(img_file, axis=2)
         k = 2
         depth = 2
@@ -130,11 +134,12 @@ class UtilsTest(unittest.TestCase):
             # print(line)
             x = [line[0][0], line[1][0]]
             y = [line[0][1], line[1][1]]
-            plt.plot(x,y, linewidth=1, color='red')
+            plt.plot(x, y, linewidth=1, color='red')
         plt.show()
 
-    def test_with_bar_code_1(self):
-        img_file = imageio.imread('/home/ge/k-segment/datasets/bar_code.png', )
+    @staticmethod
+    def test_with_bar_code_1():
+        img_file = imageio.imread('/home/ge/k-segment/datasets/segmentation/bar_code.png', )
         flat_img = np.mean(img_file, axis=2)
         k = 1
         depth = 2
